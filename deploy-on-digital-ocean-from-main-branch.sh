75794e9a7ad0084144f6a6485b9a1c7e46812905
Metaphysiker <s.raess@me.com>
2022-10-08 22:32:21 +0200

    new icons

--- deploy-on-digital-ocean-from-main-branch.sh
+++ deploy-on-digital-ocean-from-main-branch.sh
@@ -1,3 +1,13 @@
-cd stinah-diary
-#nvm use
-npm install --omit= dev
+#!/bin/bash
+ng build
+ssh sandro@159.65.120.231 << EOF
+  cd stinah-diary
+  git pull origin main
+  nvm use
+  npm install --omit=dev
+EOF
+
+ssh root@159.65.120.231 << EOF
+  cd /home/sandro/stinah-diary/
+  sudo service nginx restart
+EOF
