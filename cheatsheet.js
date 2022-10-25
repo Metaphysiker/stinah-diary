cbab7424869483ecfc3e4bff54cacb725ea7a73a
Metaphysiker <s.raess@me.com>
2022-10-25 23:52:04 +0200

    updated cheatsheet

--- cheatsheet.js
+++ cheatsheet.js
@@ -8,7 +8,7 @@ db.entries.find({}).forEach(function(entry){
 db.files.find({}).forEach(function(file){
   db.files.update(
     {_id: file._id},
-    {$set:{entry: ObjectId(entry.entry_id)}}
+    {$set:{entry: ObjectId(file.entry_id)}}
 )});
 
 
