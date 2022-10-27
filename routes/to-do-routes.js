97110acefbb29721c6e42feafe9e669d2606ce9f
Metaphysiker <s.raess@me.com>
2022-10-27 09:26:27 +0200

    adding to do routes

---
+++ routes/to-do-routes.js
@@ -0,0 +1,39 @@
+const router = express.Router();
+const ToDoModel = require('../model/to_do');
+
+router.get(
+  '/to_dos',
+  async (req, res, next) => {
+
+    const filter = {};
+    const to_dos = await ToDoModel.find(filter);
+    res.json(to_dos)
+  }
+);
+
+router.post(
+  '/to_dos',
+  async (req, res, next) => {
+
+    try {
+      const to_do = await ToDoModel.create({ content: req.body.content });
+      res.json(to_do)
+    } catch (error) {
+      console.log(error);
+    }
+
+  }
+);
+
+router.delete('/to_dos/:id',
+  async (req, res, next) => {
+    const filter = {_id: req.params.id};
+    ToDoModel.deleteOne(filter, function (err) {
+      if (err) return handleError(err);
+      res.json("to_do_deleted")
+    });
+  }
+)
+
+
+module.exports = router;
