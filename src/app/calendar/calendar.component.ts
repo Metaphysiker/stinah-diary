6a00287f566db1c8de1802dcc078594c9cdbd84d
Metaphysiker <s.raess@me.com>
2022-11-04 19:00:09 +0100

    praying for better calendar

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -37,24 +37,34 @@ export class CalendarComponent implements OnInit {
   }
 
   loadCalendar(){
+
+    this.all_days = [];
+    this.entries = [];
+    this.weeks = [];
+
     this.getEntriesForCalendar()
-    .then((data)=> {
-      
+    .then((data) => {
+      this.setDaysForCalendar()
     })
+    .then((days) => {
+      this.fillWeeks();
+    });
 
-    this.setDaysForCalendar();
+    //this.setDaysForCalendar();
 
-    this.fillWeeks();
+    //this.fillWeeks();
   }
 
 
   getEntriesForCalendar(){
 
+    var self = this;
+
     return new Promise(function(final_resolve, final_reject){
 
-      this.entryService.getEntriesForCalendar(this.selected_date.toUTCString())
+      self.entryService.getEntriesForCalendar(self.selected_date.toUTCString())
       .then((data: any) => {
-        this.entries = data;
+        self.entries = data;
         final_resolve(data);
       });
 
@@ -92,68 +102,76 @@ export class CalendarComponent implements OnInit {
 
   fillWeeks(){
 
-    var week = [];
+    var week: any = [];
 
     for (let i = 0; i < this.all_days.length; i++) {
 
-      var calendar_date = {
-        date: this.all_days[i],
-        entries_exist: this.checkIfEntryExistsOnThisDate(this.all_days[i])
-      }
+      this.checkIfEntryExistsOnThisDate(this.all_days[i])
+      .then((boolean) => {
+        var calendar_date = {
+          date: this.all_days[i],
+          entries_exist: boolean
+        }
 
-      week.push(calendar_date);
-      if(((i + 1) % 7)==0){
-        this.weeks.push(week);
-        week = [];
-      }
+        week.push(calendar_date);
+        if(((i + 1) % 7)==0){
+          this.weeks.push(week);
+          week = [];
+        }
+      })
+
     }
 
   }
 
   checkIfEntryExistsOnThisDate(date: any){
-    console.log("checkIfEntryExistsOnThisDatexxx");
-    console.log(date);
+    var self = this;
 
-    for (let i = 0; i < this.entries.length; i++) {
+    return new Promise(function(final_resolve, final_reject){
 
-      console.log(this.entries[i]?.createdAt?.getFullYear());
-      console.log(this.selected_date.getFullYear());
+      for (let i = 0; i < self.entries.length; i++) {
 
-      if (
-          this.entries[i]?.createdAt?.getFullYear() === this.selected_date.getFullYear() &&
-          this.entries[i]?.createdAt?.getMonth() === this.selected_date.getMonth() &&
-          this.entries[i]?.createdAt?.getDate() === this.selected_date.getDate()
-        ){
-          return true;
-        }
+        if (
+            self.entries[i]?.createdAt?.getUTCFullYear() === date.getUTCFullYear() &&
+            self.entries[i]?.createdAt?.getUTCMonth() === date.getUTCMonth() &&
+            self.entries[i]?.createdAt?.getUTCDate() === date.getUTCDate()
+          ){
+            final_resolve(true);
+          }
 
-    }
+          if((i+1) <= self.entries.length ){
+            final_resolve(false);
+          }
 
-    return false;
+      }
+
+    })
+
   }
 
   setDaysForCalendar(){
 
+    var self = this;
+
     return new Promise(function(final_resolve, final_reject){
-      this.firstDayOfMonth = new Date(this.selected_date.getFullYear(), this.selected_date.getMonth(), 1);
-      this.lastDayOfMonth = new Date(this.selected_date.getFullYear(), this.selected_date.getMonth() + 1, 0);
+      self.firstDayOfMonth = new Date(self.selected_date.getFullYear(), self.selected_date.getMonth(), 1);
+      self.lastDayOfMonth = new Date(self.selected_date.getFullYear(), self.selected_date.getMonth() + 1, 0);
 
-      this.firstDay = this.getFirstDayOfWeek(this.firstDayOfMonth);
-      this.lastDay = this.getLastDayOfWeek(this.lastDayOfMonth);
+      self.firstDay = self.getFirstDayOfWeek(self.firstDayOfMonth);
+      self.lastDay = self.getLastDayOfWeek(self.lastDayOfMonth);
+      final_resolve("");
 
-      this.all_days = [];
-      this.weeks = [];
+      let loop = new Date(self.firstDay);
 
-      let loop = new Date(this.firstDay);
+      while (loop <= self.lastDay) {
 
-      while (loop <= this.lastDay) {
-        console.log(loop);
-        this.all_days.push(loop);
+        self.all_days.push(loop);
+
         let newDate = loop.setDate(loop.getDate() + 1);
 
         loop = new Date(newDate);
 
-        if(newDate <= this.lastDay){
+        if(newDate <= self.lastDay){
           final_resolve("");
         }
 
@@ -164,13 +182,11 @@ export class CalendarComponent implements OnInit {
   }
 
   getPreviousMonth(){
-    console.log("previous");
     this.selected_date = new Date(Date.UTC(this.selected_date.getFullYear(), this.selected_date.getMonth()-1,1));
     this.loadCalendar();
   }
 
   getNextMonth(){
-    console.log("next");
     this.selected_date = new Date(Date.UTC(this.selected_date.getFullYear(), this.selected_date.getMonth()+1,1));
     this.loadCalendar();
   }
